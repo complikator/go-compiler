8067bc3b28934a71484d46bbad4d253591cee683
Paweł Łacek <pawel.lacek@robotec.ai>
2026-02-16 00:23:00 +0100

    compilation: Allocate variables and structures

--- compilation.ml
+++ compilation.ml
@@ -1,6 +1,7 @@
 open Tast
 open X86_64
 open Ast
+module StringSet = Set.Make (String)
 
 module CompilationUtils = struct
   let fold_left_concat f = List.fold_left (fun acc x -> acc ++ f x) nop
@@ -43,6 +44,10 @@ module Constants = struct
   let function_printf_label = "printf_"
   let function_malloc_label = "malloc_"
   let function_calloc_label = "calloc_"
+end
+
+module SizeConstants = struct
+  let standard_field_size_bytes = 8
 end
 
 (* String table to manage string constants globally *)
@@ -145,6 +150,106 @@ module Data = struct
     generate_string_constants () ++ generate_format_constants ()
 end
 
+module Allocation = struct
+  (* Calculates offset of fields and size of entire structure *)
+  let allocate_all_structures (tfile : Tast.tfile) : unit =
+    let allocated_structures = ref StringSet.empty in
+
+    let sizeof (t : typ) : int =
+      match t with
+      | Tstruct s -> s.s_size
+      | _ -> SizeConstants.standard_field_size_bytes
+    in
+
+    (* recursively calculates size of structures and its fields *)
+    let rec allocate_structure (s : structure) : unit =
+      if StringSet.mem s.s_name !allocated_structures then ()
+      else begin
+        let offset = ref 0 in
+
+        (* process all fields *)
+        List.iter
+          (fun f ->
+            match f.f_typ with
+            | Tstruct nested -> allocate_structure nested
+            | _ ->
+                ();
+
+                f.f_ofs <- !offset;
+                offset := !offset + sizeof f.f_typ)
+          s.s_list;
+
+        s.s_size <- !offset;
+        allocated_structures := StringSet.add s.s_name !allocated_structures
+      end
+    in
+
+    List.iter
+      (fun td -> match td with TDstruct s -> allocate_structure s | _ -> ())
+      tfile;
+
+    ()
+
+  (* traverses program tree and allocates variables on the updates offsets *)
+  (* returns total size of the stack *)
+  let allocate_variables (body : expr) : int =
+    let offset = ref 0 in
+
+    let rec visit_expr (e : Tast.expr) : unit =
+      match e.expr_desc with
+      | TEvars var_list ->
+          List.iter
+            (fun v ->
+              offset := !offset - 8;
+              v.v_ofs <- !offset)
+            var_list
+      | TEbinop (_, e1, e2) ->
+          visit_expr e1;
+          visit_expr e2
+      | TEunop (_, e) -> visit_expr e
+      | TEcall (_, exprs) -> List.iter visit_expr exprs
+      | TEdot (e, _) -> visit_expr e
+      | TEassign (lhs, rhs) ->
+          List.iter visit_expr lhs;
+          List.iter visit_expr rhs
+      | TEif (e1, e2, e3) ->
+          visit_expr e1;
+          visit_expr e2;
+          visit_expr e3
+      | TEreturn exprs -> List.iter visit_expr exprs
+      | TEblock exprs -> List.iter visit_expr exprs
+      | TEfor (e1, e2) ->
+          visit_expr e1;
+          visit_expr e2
+      | TEprint exprs -> List.iter visit_expr exprs
+      | TEincdec (e, _) -> visit_expr e
+      | _ -> ()
+    in
+
+    visit_expr body;
+
+    - !offset
+
+  let allocate_function_params (fn : function_) : unit =
+    (* parameters are on the other side of rbp *)
+    (* that's why they have positive offsets *)
+    (* initial value is 16 because we skip return address and previous rbp *)
+    let offset = ref 16 in
+    List.iter
+      (fun param ->
+        param.v_ofs <- !offset;
+        offset := !offset + 8)
+      fn.fn_params;
+    ()
+
+  (* calculates offsets for all the variables and parameters *)
+  let allocate_function (fn : function_) (body : expr) : int =
+    allocate_function_params fn;
+
+    (* allocate local variables *)
+    allocate_variables body
+end
+
 module Compilation = struct
   let constant (e : Tast.constant) : text =
     match e with
@@ -159,7 +264,13 @@ module Compilation = struct
     match op with
     | Uneg -> compile_expr e ++ negq (reg rax)
     | Unot -> compile_expr e ++ BoolOps.generate_negation_code
-    | _ -> assert false (* TODO: compile the rest *)
+    | Uamp -> (
+        match e.expr_desc with
+        | TEident v -> leaq (ind ~ofs:v.v_ofs rbp) rax
+        | TEdot (ee, field) ->
+            compile_expr ee ++ leaq (ind ~ofs:field.f_ofs rax) rax
+        | _ -> failwith "Cannot take address of non-variable expression")
+    | Ustar -> compile_expr e ++ movq (ind rax) (reg rax)
 
   let print (compile_expr : expr -> text) (expr_list : expr list) : text =
     CompilationUtils.fold_left_concat
@@ -201,7 +312,7 @@ module Compilation = struct
             ++ leaq (lab Constants.format_string_label) rdi
             ++ movq (reg rax) (reg rsi)
             ++ call Constants.function_printf_label
-        | _ -> assert false)
+        | _ -> failwith "Unsupported type for print")
       expr_list
 
   let block (compile_expr : expr -> text) (expr_list : expr list) : text =
@@ -213,9 +324,11 @@ module Compilation = struct
     | TEunop (op, e) -> unop compile_expr op e
     | TEprint expr_list -> print compile_expr expr_list
     | TEblock expr_list -> block compile_expr expr_list
-    | _ -> nop (* TODO: compile other expressions *)
+    | _ -> failwith "Unsupported expression type"
 
   let compile_function (fn : function_) (body : expr) : text =
+    let local_stack_size = Allocation.allocate_function fn body in
+
     label fn.fn_name
     ++ pushq (reg rbp)
     ++ movq (reg rsp) (reg rbp)
